93da155f3917ef1d1187d75e63257f5820730526
aswanivinita <55902733+aswanivinita@users.noreply.github.com>
2019-09-27 19:35:15 -0700

    TestCommitt

---
+++ TestMatrimonial/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestMatrimonial
+{
+    class Person
+    {
+        
+        public enum ReligionType
+        {
+            Hindu, 
+            Islamic,
+            Christian
+        };
+        #region Properties
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+        public ReligionType Religion { get; set; }
+        public string WorkStatus { get; set; }
+        public double AnnualIncome { get; set; }
+        public int Age{ get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        public Person(DateTime dob)
+        {
+            Console.WriteLine("calling constructor ");
+            Console.WriteLine(DOB.ToString());
+                 
+        }
+
+        #endregion
+
+
+
+    }
+}
---
+++ TestMatrimonial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TestMatrimonial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            var Person1 = new Person
+            {
+                Name = "zzz",
+                DOB = new DateTime(1983, 7, 22),
+                Religion = Person.ReligionType.Christian,
+                WorkStatus = "Self Employed",
+                AnnualIncome = 20000.45
+            
+
+
+            };
+            
+        }
+    }
+}
